4d0b851da14f0efe58698237c48ebf6b498dac39
felipecespedes <felipecespedespisso@gmail.com>
2021-02-06 18:51:53 -0500

    implements basic price checker

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import axios from 'axios';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,15 +14,50 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dogecoin-price-checker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  let checkPrice = vscode.commands.registerCommand('dogecoin-price-checker.checkPrice', async () => {
+    try {
+      const API_URL = 'https://sochain.com/api/v2/get_price/DOGE/USD';
+      const response = await axios.get(API_URL);
+      if (response.status === 200) {
+        const prices = response.data?.data?.prices;
+        let price;
+        if (Array.isArray(prices) && prices.length > 0) {
+          price = prices[0].price;
+        } else {
+          displayError('no prices');
+        }
+        if (price != null) {
+          displayPrice(price);
+        } else {
+          displayError('price is null');
+        }
+      } else {
+        displayError('response status !== 200');
+      }
+    } catch (err) {
+      displayError(err);
+    }
+  });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Checking Dogecoin price...');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(checkPrice);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+// Custom functions
+function displayError(err: any) {
+  console.error(err);
+  vscode.window.showErrorMessage('There was an error while getting the Dogecoin price, plese try again in few minutes');
+}
+
+function displayPrice(price: string) {
+  // const buyOption = 'Buy Dogecoin in Binance';
+  // vscode.window.showInformationMessage(`Dogecoing price: $${price} USD`, buyOption).then(selection => {
+  //   if (selection === buyOption) {
+  //     vscode.env.openExternal(vscode.Uri.parse('https://www.binance.com/en/register?ref=GZOBAXCR'));
+  //   }
+  // });
+
+  vscode.window.showInformationMessage(`Dogecoing price: $${price} USD`);
+}
